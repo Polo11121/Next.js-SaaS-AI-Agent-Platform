940ef9a88e4fcc852af1db5742531c4e4376249b
Polo1121 <michal.jasinski11121@gmail.com>
2025-07-02 20:58:46 +0200

    Add inngest

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,12 +1,16 @@
 import {
+  CallSessionEndedEvent,
   CallSessionParticipantLeftEvent,
   CallSessionStartedEvent,
+  CallTranscriptionReadyEvent,
+  CallRecordingReadyEvent,
 } from "@stream-io/node-sdk";
 import { streamVideo } from "@/lib/stream-video";
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/db";
 import { agents, meetings } from "@/db/schema";
 import { and, eq, not } from "drizzle-orm";
+import { inngest } from "@/inngest/client";
 
 export const verifySignatureWithSD = (body: string, signature: string) =>
   streamVideo.verifyWebhook(body, signature);
@@ -99,6 +103,49 @@ export const POST = async (req: NextRequest) => {
     const call = streamVideo.video.call("default", meetingId);
 
     await call.end();
+  } else if (eventType === "call.session_ended") {
+    const event = payload as CallSessionEndedEvent;
+    const meetingId = event.call.custom?.meetingId;
+
+    if (!meetingId) {
+      return NextResponse.json({ error: "Missing meetingId" }, { status: 400 });
+    }
+
+    await db
+      .update(meetings)
+      .set({ status: "processing", endedAt: new Date() })
+      .where(and(eq(meetings.id, meetingId), eq(meetings.status, "active")));
+  } else if (eventType === "call.transcription_ready") {
+    const event = payload as CallTranscriptionReadyEvent;
+    const meetingId = event.call_cid.split(":")[1];
+
+    const [updatedMeeting] = await db
+      .update(meetings)
+      .set({ transcriptUrl: event.call_transcription.url })
+      .where(eq(meetings.id, meetingId))
+      .returning();
+
+    if (!updatedMeeting) {
+      return NextResponse.json({ error: "Meeting not found" }, { status: 404 });
+    }
+
+    await inngest.send({
+      name: "meetings/processing",
+      data: {
+        meetingId: updatedMeeting.id,
+        transcriptUrl: updatedMeeting.transcriptUrl,
+      },
+    });
+  } else if (eventType === "call.recording_ready") {
+    const event = payload as CallRecordingReadyEvent;
+    const meetingId = event.call_cid.split(":")[1];
+
+    await db
+      .update(meetings)
+      .set({
+        recordingUrl: event.call_recording.url,
+      })
+      .where(eq(meetings.id, meetingId));
   }
 
   return NextResponse.json({ message: "Webhook received" }, { status: 200 });
